ce0f1ea98930db46bbd9004459ce3e01e94a50d4
haleyej <haleyej@umich.edu>
2023-11-30 17:34:38 -0500

    vector ranker implemented

--- code/vector_ranker.py
+++ code/vector_ranker.py
@@ -2,6 +2,7 @@ from sentence_transformers import SentenceTransformer, util
 from numpy import ndarray
 from ranker import Ranker
 import numpy as np
+from itertools import combinations
 
 
 class VectorRanker(Ranker):
@@ -23,10 +24,15 @@ class VectorRanker(Ranker):
 
         # NOTE: we're going to use the cpu for everything here so if you decide to use a GPU, do not 
         # submit that code to the autograder
-        pass
+        self.bi_encoder_model = SentenceTransformer(bi_encoder_model_name, device = 'cpu')
+        self.encoded_docs = encoded_docs
+        self.row_to_docid = row_to_docid
+        #docids = [docid for docid, _ in ]
+        self.mapping = dict(zip(row_to_docid, encoded_docs))
+        
 
     def query(self, query: str, pseudofeedback_num_docs=0,
-              pseudofeedback_alpha=0.8, pseudofeedback_beta=0.2, user_id=None) -> list[tuple[int, float]]:
+              pseduofeedback_alpha=0.8, pseduofeedback_beta=0.2, user_id = None) -> list[tuple[int, float]]:
         """
         Encodes the query and then scores the relevance of the query with all the documents.
         Performs query expansion using pseudo-relevance feedback if needed.
@@ -35,38 +41,35 @@ class VectorRanker(Ranker):
             query: The query to search for
             pseudofeedback_num_docs: If pseudo-feedback is requested, the number of top-ranked documents
                 to be used in the query
-            pseudofeedback_alpha: If pseudo-feedback is used, the alpha parameter for weighting
+            pseduofeedback_alpha: If pseudo-feedback is used, the alpha parameter for weighting
                 how much to include of the original query in the updated query
-            pseudofeedback_beta: If pseudo-feedback is used, the beta parameter for weighting
+            pseduofeedback_beta: If pseudo-feedback is used, the beta parameter for weighting
                 how much to include of the relevant documents in the updated query
-            user_id: We don't use the user_id parameter in vector ranker. It is here just to align all the
-                    Ranker interfaces.
 
         Returns:
             A sorted list of tuples containing the document id and its relevance to the query,
             with most relevant documents first
         """
-        pass
-        # NOTE: Do not forget to handle edge cases on the input
+        encoded_query = self.bi_encoder_model.encode(query)
+        scores = util.dot_score(encoded_query, self.encoded_docs)[0].cpu().tolist()
+        scores_and_ids = []
+        [scores_and_ids.append((tup, scores[i])) for i, tup in enumerate(self.row_to_docid)]
+        scores_and_ids = sorted(scores_and_ids, key = lambda s: s[1], reverse = True)
 
-        # TODO: Encode the query using the bi-encoder
 
-        # TODO (HW4): If the user has indicated we should use feedback, then update the
-        #  query vector with respect to the specified number of most-relevant documents
+        if pseudofeedback_num_docs > 0:
+            rel_docs = scores_and_ids[:pseudofeedback_num_docs]
+            rel_doc_vecs = np.array([self.mapping[docid] for docid, _ in rel_docs])
+            avg_doc_vec = np.mean(rel_doc_vecs, axis = 0)
+            modified_query = (pseduofeedback_alpha * encoded_query) + (pseduofeedback_beta * avg_doc_vec)
 
-            # TODO (HW4): Get the most-relevant document vectors for the initial query
+            scores = util.dot_score(modified_query, self.encoded_docs)[0].cpu().tolist()
+            scores_and_ids = []
+            [scores_and_ids.append((tup, scores[i])) for i, tup in enumerate(self.row_to_docid)]
+            scores_and_ids = sorted(scores_and_ids, key = lambda s: s[1], reverse = True)
 
-            # TODO (HW4): Compute the average vector of the specified number of most-relevant docs
-            #  according to how many are to be used for pseudofeedback
+        return scores_and_ids
 
-            # TODO (HW4): Combine the original query doc with the feedback doc to use
-            #  as the new query embedding
-
-        # TODO: Score the similarity of the query vec and document vectors for relevance
-
-        # TODO: Generate the ordered list of (document id, score) tuples
-
-        # TODO: Sort the list by relevance score in descending order (most relevant first)
 
     # TODO (HW5): Find the dot product (unnormalized cosine similarity) for the list of documents (pairwise)
     # NOTE: You should return a matrix where element [i][j] would represent similarity between
@@ -81,5 +84,14 @@ class VectorRanker(Ranker):
         Returns:
             A matrix where element [i][j] is a similarity score between list_docs[i] and list_docs[j]
         """
-        pass
+        dot_scores = []
+        for doc_1 in list_docs:
+            for doc_2 in list_docs:
+                doc_1_embed = self.mapping[doc_1]
+                doc_2_embed = self.mapping[doc_2]
+                dot_score = util.dot_score(doc_1_embed, doc_2_embed)[0].cpu().tolist()
+                dot_scores.append(dot_score[0])
+        return np.array(dot_scores).reshape(len(list_docs), len(list_docs))
+
+        
 
